d79edd69cda16ce5081200aa713b74c57b7c0df9
Mohit Jaiswal <mohitjaiswal9753589@gmail.com>
2026-08-02 22:18:59 +0545

    Add Telegram bot to send NEPSE scanner message

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import os
+import requests
+from telegram import Bot
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+bot = Bot(token=BOT_TOKEN)
+
+message = """
+📈 NEPSE Scanner
+
+Bot is running ✅
+
+Market scan will be added soon.
+"""
+
+bot.send_message(chat_id=CHAT_ID, text=message)
