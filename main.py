109d057fd03857c4348b490a6ba2f8d54871f529
Mohit Jaiswal <mohitjaiswal9753589@gmail.com>
2026-08-03 07:54:57 +0545

    Refactor to use async main function for API call

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
+import os
 import requests
+import asyncio
+from telegram import Bot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 API_URL = "https://nepse-data-api.vercel.app/api/history"
 
-def test_data():
-    symbol = "NABIL"
+async def main():
 
-    response = requests.get(
-        API_URL,
-        params={"symbol": symbol}
+    try:
+        response = requests.get(
+            API_URL,
+            params={"symbol": "NABIL"},
+            timeout=10
+        )
+
+        data = response.text[:500]
+
+        message = (
+            "📊 NEPSE API Test\n\n"
+            f"{data}"
+        )
+
+    except Exception as e:
+        message = f"❌ API Error:\n{e}"
+
+    bot = Bot(token=BOT_TOKEN)
+
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text=message
     )
 
-    print(response.status_code)
-    print(response.text[:500])
 
-test_data()
+if __name__ == "__main__":
+    asyncio.run(main())
